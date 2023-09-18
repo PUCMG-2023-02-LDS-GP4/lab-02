18e81e4256bf4959a2600b007537ba1aa33843d8
luanaf4 <luana.fleury4@gmail.com>
2023-09-17 23:27:35 -0300

    Inicializando projeto #7

---
+++ aluguelveiculos/src/main/java/com/lab02/aluguelveiculos/AluguelveiculosApplication.java
@@ -0,0 +1,15 @@
+package com.lab02.aluguelveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelveiculosApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelveiculosApplication.class, args);
+	}
+
+
+}
---
+++ aluguelveiculos/src/test/java/com/lab02/aluguelveiculos/AluguelveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab02.aluguelveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
